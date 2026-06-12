a6d28a950fc9357bfd5775d8a5663c2f90612e98
Debora Serra <debora.r.serra@gmail.com>
2026-06-12 15:08:40 -0700

    refactor: create and apply factory for clients - entity link selector

---
+++ Clients/src/test/factories/index.ts
@@ -0,0 +1,6 @@
+export { buildOrganization, buildManyOrganization } from "./organization.factory";
+export { buildUser, buildManyUser } from "./user.factory";
+export { buildProject, buildManyProject } from "./project.factory";
+export { buildRisk, buildManyRisk } from "./risk.factory";
+export { buildTask, buildManyTask } from "./task.factory";
+export { buildVendor, buildManyVendor } from "./vendor.factory";
